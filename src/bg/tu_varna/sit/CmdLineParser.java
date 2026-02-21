5acb9ac5c2aea3f27c962d121860b3bbdc3621a5
Denis Georgiev <d1bg@tuta.io>
2026-02-21 21:46:51 +0200

    feat: added help message

--- src/bg/tu_varna/sit/CmdLineParser.java
+++ src/bg/tu_varna/sit/CmdLineParser.java
@@ -7,7 +7,6 @@ public class CmdLineParser {
             case "close":
             case "save":
             case "saveas":
-            case "help":
             case "addsong":
             case "removesong":
             case "listsongs":
@@ -28,6 +27,9 @@ public class CmdLineParser {
             case "dropplaylist":
                 System.out.println("Command not implemented yet");
                 break;
+            case "help":
+                printHelp();
+                break;
             case "exit":
                 System.out.println("Exiting...");
                 System.exit(0);
@@ -35,5 +37,83 @@ public class CmdLineParser {
             default:
                 System.out.println("Command " + cmdLine[0] + " not found, do 'help' for available commands");
         }
+    }
+
+    public void printHelp() {
+        String helpMessage = """
+            All Available Commands:
+                open <file>
+                    Отваря файл със записани данни.
+            
+                close
+                    Затваря текущия файл.
+            
+                save
+                    Записва промените в текущия файл.
+            
+                saveas <file>
+                    Записва данните в нов файл.
+            
+                help
+                    Изважда това съобщение.
+            
+                exit
+                    Прекратява програмата.
+            
+                addsong <title> <artist> <duration> [<album>] [<year>] [<genre>]
+                    Добавя нова песен. Ако съществува песен със същото <title> и <artist>, връща грешка.
+            
+                removesong <songId>
+                    Изтрива песен по ID.
+            
+                listsongs [artist=<artist>] [genre=<genre>] [year=<year>]
+                    Извежда списък с песни с възможност за филтриране.
+            
+                songinfo <songId>
+                    Показва подробна информация за песен.
+            
+                createplaylist <name> [<description>]
+                    Създава нов плейлист с уникално име.
+            
+                deleteplaylist <name>
+                    Изтрива плейлист.
+            
+                addtoplaylist <playlistName> <songId> [pos=<n>]
+                    Добавя песен към плейлист. Ако е зададена позиция – добавя я там.
+            
+                removefromplaylist <playlistName> <songId>
+                    Премахва песен от плейлист.
+            
+                move <playlistName> <fromPos> <toPos>
+                    Премества песен в рамките на плейлист.
+            
+                showplaylist <playlistName>
+                    Извежда съдържанието на плейлиста и общата му продължителност.
+            
+                shuffle <playlistName> [seed=<n>]
+                    Разбърква песните в плейлист.
+            
+                play <songId> [playlist=<playlistName>]
+                    Отбелязва слушане на песен (записва дата и час).
+            
+                plays [from=<date>] [to=<date>] [playlist=<name>] [song=<songId>]
+                    Справка за слушанията според зададени критерии.
+            
+                toptracks <n> [from=<date>] [to=<date>]
+                    Извежда топ n най-слушани песни за даден период.
+            
+                topplaylists <n> [from=<date>] [to=<date>]
+                    Извежда топ n най-активни плейлисти.
+            
+                topartists <n> [from=<date>] [to=<date>]
+                    Извежда топ n изпълнители по слушания.
+            
+                lowactivity <from> <to> <thresholdPercent>
+                    Извежда плейлисти с активност под зададения процент за периода.
+            
+                dropplaylist <playlistName>
+                    Премахва плейлист от системата.
+            """;
+        System.out.print(helpMessage);
     }
 }
