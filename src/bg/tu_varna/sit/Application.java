d9655c5cbdd276cb55697d584fb5f37419193b9c
Denis Georgiev <d1bg@tuta.io>
2026-02-21 20:26:51 +0200

    feat: added early implementation of the main loop and an exit command

--- src/bg/tu_varna/sit/Application.java
+++ src/bg/tu_varna/sit/Application.java
@@ -1,7 +1,20 @@
 package bg.tu_varna.sit;
 
-public class Application {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Application {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean isExiting = false;
+        do {
+            System.out.print("Music Playlist > ");
+            String[] cmd = br.readLine().split(" ");
+
+            if (cmd[0].equals("exit")) {
+                isExiting = true;
+            }
+        } while (!isExiting);
     }
 }
