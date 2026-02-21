e96fc0b671b610b3697080891af948ce6bb061b4
Denis Georgiev <d1bg@tuta.io>
2026-02-21 20:14:22 +0200

    feat: added class song with its attributes

---
+++ src/bg/tu_varna/sit/Song.java
@@ -0,0 +1,11 @@
+package bg.tu_varna.sit;
+
+public class Song {
+    private String ID;
+    private String title;
+    private String artist;
+    private String album;
+    private String duration;
+    private String year;
+    private String genre;
+}
