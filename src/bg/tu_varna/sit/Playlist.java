6ec489a65ef59705b4674e5f8277dcf242df54da
Denis Georgiev <d1bg@tuta.io>
2026-02-23 12:10:29 +0200

    feat: added a constructor, getters and equals methods to the Playlist class

--- src/bg/tu_varna/sit/Playlist.java
+++ src/bg/tu_varna/sit/Playlist.java
@@ -1,9 +1,40 @@
 package bg.tu_varna.sit;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Playlist {
     private String name;
     private String description;
     private List<Song> songs;
+
+    public Playlist(String name){
+        this.name = name;
+        this.description = "N/A";
+    }
+
+    public Playlist(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Playlist playlist = (Playlist) o;
+        return Objects.equals(name, playlist.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
 }
