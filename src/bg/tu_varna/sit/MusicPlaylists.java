879b4c7cb62a5ce1eef96dbd3a28cc5570dccc85
Denis Georgiev <d1bg@tuta.io>
2026-02-23 14:06:41 +0200

    feat: implemented methods for adding a song and listing them out

--- src/bg/tu_varna/sit/MusicPlaylists.java
+++ src/bg/tu_varna/sit/MusicPlaylists.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 public class MusicPlaylists {
     private List<Playlist> playlists;
+    private List<Song> songs;
+
     public MusicPlaylists() {
         this.playlists = new ArrayList<>();
+        this.songs = new ArrayList<>();
     }
 
     public void createPlaylist(String name){
@@ -42,6 +45,57 @@ public class MusicPlaylists {
     public void listPlaylists(){
         for (Playlist playlist : playlists) {
             System.out.println(playlist.getName());
+        }
+    }
+
+    void addSong(String title, String artist, String duration){
+        for  (Song song : songs) {
+            if (song.getTitle().equals(title) && song.getArtist().equals(artist)) {
+                System.out.println("Song already exists");
+                return;
+            }
+        }
+        String songID = String.valueOf(songs.size()+1);
+        songs.add(new Song(songID, title, artist, duration));
+    }
+
+    void addSong(String title, String artist, String duration, String album){
+        for  (Song song : songs) {
+            if (song.getTitle().equals(title) && song.getArtist().equals(artist)) {
+                System.out.println("Song already exists");
+                return;
+            }
+        }
+        String songID = String.valueOf(songs.size()+1);
+        songs.add(new Song(songID, title, artist, duration, album));
+    }
+
+    void addSong(String title, String artist, String duration, String album, String year){
+        for  (Song song : songs) {
+            if (song.getTitle().equals(title) && song.getArtist().equals(artist)) {
+                System.out.println("Song already exists");
+                return;
+            }
+        }
+        String songID = String.valueOf(songs.size()+1);
+        songs.add(new Song(songID, title, artist, duration, album, year));
+    }
+
+    void addSong(String title, String artist, String duration, String album, String year, String genre){
+        for  (Song song : songs) {
+            if (song.getTitle().equals(title) && song.getArtist().equals(artist)) {
+                System.out.println("Song already exists");
+                return;
+            }
+        }
+        String songID = String.valueOf(songs.size()+1);
+        songs.add(new Song(songID, title, artist, duration, album, year, genre));
+    }
+
+    // TODO: Filtering by artist, genre, year
+    public void listSongs(){
+        for (Song song : songs) {
+            System.out.println(song.toString());
         }
     }
 }
