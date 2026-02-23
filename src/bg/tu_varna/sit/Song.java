d001cf84770b86acd21f06f851fdf3282ece9b43
Denis Georgiev <d1bg@tuta.io>
2026-02-23 13:59:18 +0200

    fix: use overloading instead of optional vars array

--- src/bg/tu_varna/sit/Song.java
+++ src/bg/tu_varna/sit/Song.java
@@ -13,42 +13,38 @@ public class Song {
     private String year;
     private String genre;
 
-    /*
-        https://stackoverflow.com/questions/965690/how-do-i-use-optional-parameters-in-java
-        I used an array of optional arguments, the other option was to use Overloading, but
-        I didnt want to repeat code and have 4 different constructors for essentially the
-        same thing
-     */
-
-    public Song(String ID, String title, String artist, String duration, String... args) {
+    public Song(String ID, String title, String artist, String duration) {
         this.ID = ID;
         this.title = title;
         this.artist = artist;
         this.duration = duration;
-        if (args.length > 0) {
-            if (args[0] != null) {
-                this.album = args[0];
-            } else {
-                this.album = "N/A";
-                this.year = "N/A";
-                this.genre = "N/A";
-                return;
-            }
+    }
 
-            if (args[1] != null) {
-                this.year = args[1];
-            }  else {
-                this.year = "N/A";
-                this.genre = "N/A";
-                return;
-            }
+    public Song(String ID, String title, String artist, String duration, String album) {
+        this.ID = ID;
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+        this.album = album;
+    }
 
-            if (args[2] != null) {
-                this.genre = args[2];
-            } else {
-                this.genre = "N/A";
-            }
-        }
+    public Song(String ID, String title, String artist, String duration, String album, String year) {
+        this.ID = ID;
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+        this.album = album;
+        this.year = year;
+    }
+
+    public Song(String ID, String title, String artist, String duration, String album, String year, String genre) {
+        this.ID = ID;
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+        this.album = album;
+        this.year = year;
+        this.genre = genre;
     }
 
 
