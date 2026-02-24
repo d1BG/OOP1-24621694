cb042f5c31f0a97b60c2dd659cb6dbf4494da19c
Denis Georgiev <d1bg@tuta.io>
2026-02-24 14:40:56 +0200

    chore: removed useless line
    forgot that this gives me a pointer and not a copy of the object

--- src/bg/tu_varna/sit/CreateplaylistCommand.java
+++ src/bg/tu_varna/sit/CreateplaylistCommand.java
@@ -24,7 +24,6 @@ public class CreateplaylistCommand implements Command {
             }
 
             playlists.add(newPlaylist);
-            musicPlaylists.setPlaylists(playlists);
         }
     }
 }
