3129609fa9e855f72ad82e60004877490dc93e0a
Denis Georgiev <d1bg@tuta.io>
2026-02-24 16:04:40 +0200

    chore: remove deprecated code

--- src/bg/tu_varna/sit/Application.java
+++ src/bg/tu_varna/sit/Application.java
@@ -9,21 +9,15 @@ public class Application {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         MusicPlaylists musicPlaylists = new MusicPlaylists();
-        do {
-            Commands commands = new Commands(musicPlaylists);
+        Commands commands = new Commands(musicPlaylists);
 
+        do {
             System.out.print("Music Playlist > ");
             String currentLine = br.readLine();
             boolean inQuotes = false;
 
             ArrayList<String> tokens = new ArrayList<>();
             StringBuilder currToken = new StringBuilder();
-
-             /*
-                TODO: Linux file paths can include quotes,
-                 so alongside with this, I also need to add
-                 escape characters..
-             */
 
             for (int i = 0; i < currentLine.length(); i++) {
                 switch (currentLine.charAt(i)) {
@@ -45,8 +39,6 @@ public class Application {
             tokens.add(currToken.toString()); // add the last token
 
             commands.exec(tokens.getFirst(), tokens);
-
-            // CmdLineParser parser = new CmdLineParser(tokens, musicPlaylists);
         } while (true);
     }
 }
