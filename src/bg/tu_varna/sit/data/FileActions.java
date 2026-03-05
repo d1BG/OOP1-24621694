ba4be0c7508ed3588694d8726e1188ee5eb5d261
Denis Georgiev <d1bg@tuta.io>
2026-03-05 10:50:38 +0200

    feat: Abstraction and half-implementation of FileActions

---
+++ src/bg/tu_varna/sit/data/FileActions.java
@@ -0,0 +1,13 @@
+package bg.tu_varna.sit.data;
+
+import java.io.File;
+
+public interface FileActions {
+    boolean open(File file);
+    void close();
+
+    boolean write(MusicPlaylists musicPlaylists, File file);
+
+    boolean isOpen(File file);
+    boolean isOpen();
+}
---
+++ src/bg/tu_varna/sit/data/FileService.java
@@ -0,0 +1,72 @@
+package bg.tu_varna.sit.data;
+
+
+import bg.tu_varna.sit.exceptions.FileException;
+
+import java.io.*;
+
+public class FileService implements FileActions {
+    File file;
+    public FileService(File file) {
+        this.file = file;
+    }
+
+    public FileService(){}
+
+
+    // TODO: open file should load the file
+    @Override
+    public boolean open(File file) {
+        if (isOpen()) {
+            throw new FileException("A File is already open");
+        }
+
+        if (file.exists()) {
+            this.file = file;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void close() {
+        if (!isOpen()) {
+            throw new FileException("There is no open file");
+        }
+        this.file = null;
+    }
+
+    @Override
+    public boolean write(MusicPlaylists musicPlaylists, File file) {
+        if (file == null) {
+            file = this.file; // if no file is passed, use the already opened one
+        }
+
+        if (!isOpen(file)) {
+            try {
+                file.createNewFile();
+            } catch ( IOException e ) {
+                throw new FileException("Could not create new file");
+            }
+        }
+
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(musicPlaylists);
+            return true;
+        } catch (IOException e) {
+            throw new FileException(e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean isOpen() {
+        return (file.exists() && file.isFile());
+    }
+
+    @Override
+    public boolean isOpen(File file) {
+        return (file.exists() && file.isFile());
+    }
+}
