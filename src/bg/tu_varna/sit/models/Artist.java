9bd69b1c988b78af0c477d0d5c78a65a4796d449
Denis Georgiev <d1bg@tuta.io>
2026-03-17 13:29:58 +0200

    feat: artist model

--- src/bg/tu_varna/sit/models/Artist.java
+++ src/bg/tu_varna/sit/models/Artist.java
@@ -1,4 +1,54 @@
 package bg.tu_varna.sit.models;
 
 public class Artist {
+    // must-have
+    private String username;
+    // optional
+    private String firstName;
+    private String lastName;
+
+    public Artist(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if (firstName != null) {
+            sb.append(firstName);
+            if (lastName != null) {
+                sb.append(" ").append(lastName);
+            }
+        }
+
+        if (sb.isEmpty()) {
+            sb.append(username);
+        }
+        return sb.toString();
+    }
 }
