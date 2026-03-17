7fd42c1edf35b285f6ea4289db038d73d8560175
Denis Georgiev <d1bg@tuta.io>
2026-03-17 18:44:10 +0200

    feat: play history entry model

---
+++ src/bg/tu_varna/sit/models/PlayHistoryEntry.java
@@ -0,0 +1,38 @@
+package bg.tu_varna.sit.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class PlayHistoryEntry implements Serializable {
+    private Song song;
+    private LocalDateTime timestamp;
+    private Playlist playlist; // may be null
+
+    PlayHistoryEntry(Song song, Playlist playlist) {
+        this.song = song;
+        this.timestamp = LocalDateTime.now();
+        this.playlist = playlist;
+    }
+
+    PlayHistoryEntry(Song song) {
+        this.song = song;
+        this.timestamp = LocalDateTime.now();
+        this.playlist = null;
+    }
+
+    public Song getSong() {
+        return song;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public Playlist getPlaylist() {
+        return playlist;
+    }
+
+    public void playlistDeleted(Playlist playlist) {
+        this.playlist = null;
+    }
+}
