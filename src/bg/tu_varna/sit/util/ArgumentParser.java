c610f9d0a1a9cbe6c3253543b5c6767879225890
Denis Georgiev <d1bg@tuta.io>
2026-03-23 12:09:21 +0200

    fix: force parser to only split once

--- src/bg/tu_varna/sit/util/ArgumentParser.java
+++ src/bg/tu_varna/sit/util/ArgumentParser.java
@@ -1,11 +1,16 @@
 package bg.tu_varna.sit.util;
 
+import bg.tu_varna.sit.exceptions.CommandException;
+
 import java.util.Map;
 
 public class ArgumentParser {
     // Parser for key=value type of arguments
     public static Map<String, String> KeyValueParser(String arg) {
         String[] split = arg.trim().split("=");
+        if (split.length != 2) {
+            throw new CommandException("Couldn't Parse: " + arg);
+        }
         return Map.of(split[0], split[1]);
     }
 }
