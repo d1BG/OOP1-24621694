c730d005dcb70b5bfc104234fb1992dc9fc1a2b2
Denis Georgiev <d1bg@tuta.io>
2026-04-09 10:38:12 +0300

    chore: Plays Command cleanup

--- src/bg/tu_varna/sit/commands/playHistoryCommands/PlaysCommand.java
+++ src/bg/tu_varna/sit/commands/playHistoryCommands/PlaysCommand.java
@@ -10,7 +10,6 @@ import bg.tu_varna.sit.util.ArgumentParser;
 import bg.tu_varna.sit.util.DateTimeParser;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -24,21 +23,29 @@ public class PlaysCommand extends Command {
     public String execute(List<String> args) {
         ArgumentParser.argSizeChecker(args, 0, 4);
 
-
         Map<String, String> parsedArgs = ArgumentParser.KeyValueParser(args);
-        List<PlayHistoryEntry> filteredList;
 
+        Song songFilter;
         try {
-            // TODO: ADD MORE ERROR HANDLING!!!
-
-            LocalDateTime fromFilter = parsedArgs.get("from") != null ? DateTimeParser.parse(parsedArgs.get("from")) : null;
-            LocalDateTime toFilter = parsedArgs.get("to") != null ? DateTimeParser.parse(parsedArgs.get("to")) : null;
-            Song songFilter = parsedArgs.get("song") != null ? musicPlaylists.getSongActions().getSongById(Integer.parseInt(parsedArgs.get("song"))) : null;
-            Playlist playlistFilter = parsedArgs.get("playlist") != null ? musicPlaylists.getPlaylistActions().getPlaylistByName(parsedArgs.get("playlist")) : null;
-            filteredList = musicPlaylists.getPlayHistoryActions().filterEntries(fromFilter, toFilter, playlistFilter, songFilter);
+            songFilter = parsedArgs.get("song") != null ?
+                    musicPlaylists.getSongActions().getSongById(
+                            Integer.parseInt(parsedArgs.get("song"))
+                    ) : null;
         } catch (NumberFormatException e) {
             throw new CommandException(e.getMessage());
         }
+
+        LocalDateTime toFilter = parsedArgs.get("to") != null ?
+                DateTimeParser.parse(parsedArgs.get("to")) : null;
+
+        Playlist playlistFilter = parsedArgs.get("playlist") != null ?
+                musicPlaylists.getPlaylistActions().getPlaylistByName(parsedArgs.get("playlist")) : null;
+
+        LocalDateTime fromFilter = parsedArgs.get("from") != null ?
+                DateTimeParser.parse(parsedArgs.get("from")) : null;
+
+        List<PlayHistoryEntry> filteredList =
+                musicPlaylists.getPlayHistoryActions().filterEntries(fromFilter, toFilter, playlistFilter, songFilter);
 
         StringBuilder sb = new StringBuilder();
         for (PlayHistoryEntry e : filteredList) {
